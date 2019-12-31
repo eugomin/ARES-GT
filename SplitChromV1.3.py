753a55c6e62949ba27bbe5436009158c0ab68804
eugomin <47034645+eugomin@users.noreply.github.com>
2019-12-31 17:40:19 +0100

    Add files via upload

---
+++ SplitChromV1.3.py
@@ -0,0 +1,45 @@
+###############
+## Functions ##
+###############
+
+def SplitChrom(filename):
+    F = open(filename, "r")
+    i = 0
+    R = open("Report.txt","w")
+    Genome = F.readlines()
+    Lines = ""
+    names = []
+    for line in Genome:
+        line = line.strip()
+        if len(line) == 0:
+            continue
+        if line[0] == ">":
+            R.close()
+            if line[1:20] in names:
+                i = i + 1
+                FILE = line[1:20]+ "_" + str(i) + ".txt"
+            else:
+                names.append(line[1:20])
+                FILE = line[1:20] + ".txt"
+            R = open(FILE, "w")
+            R.write(">" + FILE + "\n")
+            Lines = Lines + FILE + "\n"
+        else:
+            R.write(line)
+    R.close()
+    R = open("ChromosomeList.txt","w")
+    R.write(Lines)
+    R.close()
+
+
+###############
+## Arguments ##
+###############
+
+import sys
+
+filename = sys.argv[1]
+
+SplitChrom(filename)
+
+
